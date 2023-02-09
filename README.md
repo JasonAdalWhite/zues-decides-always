5d4e05137031d9f6db05ffbf9af45344c47fe6e5
Auezov <jasonadal354@gmail.com>
2023-02-08 19:54:06 -0500

    finished html and css webpage building

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+This is a weather tracking application!
---
+++ script/script.js
@@ -0,0 +1,8 @@
+// current date
+let $cityDate = moment().format("1111");
+$("#current-date").text($cityDate);
+
+// city search functions
+
+// search city name function
+
